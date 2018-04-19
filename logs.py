973a3021748144453994ead964b07076663e3510
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-04-19 23:06:45 +0100

    Initial, bare-bones log viewer.

---
+++ logs.py
@@ -0,0 +1,33 @@
+from parser import CrawlLogLine
+from flask import Flask, Response, request, render_template
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    log_url = request.args.get('url', default='https://raw.githubusercontent.com/ukwa/w3act/master/crawl.log', type=str)
+    return render_template('viewer.html', log_url=log_url)
+
+#
+def generate(log_url, url_filter=None):
+    i = 0
+    print(log_url)
+    r = requests.get(log_url, stream=True)
+    for line in r.iter_lines():
+        log_line = CrawlLogLine(line)
+        print(url_filter,log_line.url)
+        if not url_filter or url_filter in log_line.url:
+            print(log_line)
+            i+=1
+            yield "%s\n" % line
+            if i > 100:
+                break
+
+
+@app.route("/log")
+def log_streamer():
+    log_url = request.args.get('log_url', default='https://raw.githubusercontent.com/ukwa/w3act/master/crawl.log', type=str)
+    url_filter = request.args.get('url_filter', type=str)
+    return Response(generate(log_url, url_filter), mimetype='text/plain')
---
+++ parser.py
@@ -0,0 +1,77 @@
+import re
+from urlparse import urlparse
+
+
+class CrawlLogLine(object):
+    """
+    Parsers Heritrix3 format log files, including annotations and any additional extra JSON at the end of the line.
+    """
+    def __init__(self, line):
+        """
+        Parse from a standard log-line.
+        :param line:
+        """
+        (self.timestamp, self.status_code, self.content_length, self.url, self.hop_path, self.via,
+            self.mime, self.thread, self.start_time_plus_duration, self.hash, self.source,
+            self.annotation_string) = re.split(" +", line.strip(), maxsplit=11)
+        # Account for any JSON 'extra info' ending, strip or split:
+        if self.annotation_string.endswith(' {}'):
+            self.annotation_string = self.annotation_string[:-3]
+        elif ' {"' in self.annotation_string and self.annotation_string.endswith('}'):
+            self.annotation_string, self.extra_json = re.split(re.escape(' {"'), self.annotation_string, maxsplit=1)
+            self.extra_json = '{"%s' % self.extra_json
+        # And split out the annotations:
+        self.annotations = self.annotation_string.split(',')
+
+        # Some regexes:
+        self.re_ip = re.compile('^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
+        self.re_tries = re.compile('^\d+t$')
+        self.re_dol = re.compile('^dol:\d+') # Discarded out-links - make a total?
+
+    def stats(self):
+        """
+        This generates the stats that can be meaningfully aggregated over multiple log lines.
+        i.e. fairly low-cardinality fields.
+
+        :return:
+        """
+        stats = {
+            'lines' : '', # This will count the lines under each split
+            'status_code': self.status_code,
+            'content_type': self.mime,
+            'hop': self.hop_path[-1:],
+            'sum:content_length': self.content_length,
+            'host': self.host(),
+            'source': self.source
+        }
+        # Add in annotations:
+        for annot in self.annotations:
+            # Set a prefix based on what it is:
+            prefix = ''
+            if self.re_tries.match(annot):
+                prefix = 'tries:'
+            elif self.re_ip.match(annot):
+                prefix = "ip:"
+            # Only emit lines with annotations:
+            if annot != "-":
+                stats["%s%s" % (prefix, annot)] = ""
+        return stats
+
+    def host(self):
+        """
+        Extracts the host, depending on the protocol.
+
+        :return:
+        """
+        if self.url.startswith("dns:"):
+            return self.url[4:]
+        else:
+            return urlparse(self.url).hostname
+
+    def hour(self):
+        """
+        Rounds-down to the hour.
+
+        :return:
+        """
+        return "%s:00:00" % self.timestamp[:13]
